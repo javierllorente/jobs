43711bdf6300d8b9fe0385307b4127aac07ffde4
Javier Llorente <javier@opensuse.org>
2018-12-05 17:31:48 +0100

    New class: OBSStatus

---
+++ src/com/javierllorente/jobs/OBSStatus.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2018 Javier Llorente <javier@opensuse.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.javierllorente.jobs;
+
+/**
+ *
+ * @author javier
+ */
+public class OBSStatus extends OBSObject {
+    
+    private String code;
+    private String summary;
+    private String details;
+
+    public OBSStatus() {
+    }
+    
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    
+}
