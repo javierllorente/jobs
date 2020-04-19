3c4003bacc5cfe55373582c20f7e066ef704af27
Javier Llorente <javier@opensuse.org>
2020-04-19 22:36:00 +0200

    New class: OBSFile

---
+++ src/main/java/com/javierllorente/jobs/OBSFile.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2020 Javier Llorente <javier@opensuse.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.javierllorente.jobs;
+
+/**
+ *
+ * @author javier
+ */
+public class OBSFile {
+    private String name;
+    private String size;
+    private String lastModified;
+
+    public OBSFile() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(String lastModified) {
+        this.lastModified = lastModified;
+    }
+}
