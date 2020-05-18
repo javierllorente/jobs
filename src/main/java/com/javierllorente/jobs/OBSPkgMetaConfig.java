16321bfeeb8b3c3bcf8ff4bd7381cb077bd1d81d
Javier Llorente <javier@opensuse.org>
2020-05-18 18:11:31 +0200

    New class: OBSPkgMetaConfig

---
+++ src/main/java/com/javierllorente/jobs/OBSPkgMetaConfig.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2020 Javier Llorente <javier@opensuse.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.javierllorente.jobs;
+
+import java.net.URL;
+
+/**
+ *
+ * @author javier
+ */
+public class OBSPkgMetaConfig extends OBSMetaConfig {
+    private String project;
+    private URL url;
+
+    public OBSPkgMetaConfig() {
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public void setProject(String project) {
+        this.project = project;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+    
+}
