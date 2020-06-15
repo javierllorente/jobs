7afb1240b00bae2d82956a8eb4087af802a212bc
Javier Llorente <javier@opensuse.org>
2020-06-15 21:39:57 +0200

    OBSHttp: set default timeout to 20 seconds

--- src/main/java/com/javierllorente/jobs/net/OBSHttp.java
+++ src/main/java/com/javierllorente/jobs/net/OBSHttp.java
@@ -88,6 +88,8 @@ public class OBSHttp {
     private void setupConnection(HttpsURLConnection connection) {
         connection.addRequestProperty("User-Agent", UserAgent.FULL);
         connection.setRequestProperty("Accept", "application/xml");
+        connection.setConnectTimeout(20000);
+        connection.setReadTimeout(20000);
     }
     
 }
