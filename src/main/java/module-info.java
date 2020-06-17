48fc945fb48c0f914d7e5e5df99077a886163c0a
Javier Llorente <javier@opensuse.org>
2020-06-17 11:36:37 +0200

    Add java.logging dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.javierllorente.jobs {
     requires java.naming;
     requires java.xml;
+    requires java.logging;
     exports com.javierllorente.jobs;
     exports com.javierllorente.jobs.entity;
 }
